75f22855d0cd0d5771223a7e3ed8cb0f4b0474cd
Edson Vieira Demetrio <edsonvieirademetrio@gmail.com>
2020-01-07 22:05:30 -0200

    create variables

--- src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
+++ src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { privateEncrypt } from 'crypto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JogoDaVelhaService {
+
+  //Define o tamanho do tabuleiro
+  private readonly TAM_TAB: number = 3
+  //Define um jogador
+  private readonly X: number = 1
+  //Define outro jogador
+  private readonly 0: number = 2
+  //Terceiro Estado
+  private readonly VAZIO: number = 0
+
+  //Array do tabuleiro
+  private tabuleiro: any
+  //Obtem o número de movimentos
+  private numMovimentos: number
+  //Obtem a Vitória
+  private vitoria: any
+
+  //Exibe a msg do ganhador e perdedor
+  private _jogador: number
+  //Primeiro estado
+  private _showInicio: boolean
+  //Segundo estado
+  private _showTabuleiro: boolean
+  //Terceiro Estado
+  private _showFinal: boolean
+
+
 
   constructor() { }
 }
